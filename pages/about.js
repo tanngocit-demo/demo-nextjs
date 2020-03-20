472b4e1e236de0936c480b463c88418980dcab65
Tan Nguyen <tanngocit@gmail.com>
2020-03-20 20:23:19 +0700

    add temp style

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,17 @@
-import SeoMeta from '../components/SeaoMeta';
-import Link from 'next/link';
+import SeoMeta from "../components/SeaoMeta";
+import Link from "next/link";
 
 export default function About() {
-    return (
-      <div>
-          <SeoMeta title="About Page here" description="About Page description here"/>
-        <p>This is the about page</p>
-        <Link href="/index">
-          <a>Home Page</a>
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <SeoMeta
+        title="About Page here"
+        description="About Page description here"
+      />
+      <p>This is the about page</p>
+      <Link href="/index">
+        <a>Home Page</a>
+      </Link>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Link from 'next/link';
-import SeoMeta from '../components/SeaoMeta';
+import Link from "next/link";
+import SeoMeta from "../components/SeaoMeta";
 
 export default function Index() {
-    return (
-        <div>
-            <SeoMeta/>
-        <Link href="/about">
-          <a>About Page</a>
-        </Link>
-        <p>Hello Next.js</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <SeoMeta />
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
+      <p>Hello Next.js</p>
+    </div>
+  );
+}
